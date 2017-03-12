44570e0f6630533876922e8c331f92914bd21d24
KienHT <kienhantrung@gmail.com>
2017-03-13 01:52:21 +0700

    kienht  - add Barcode Scanner activity

--- CheckSheet/app/src/main/java/com/example/ngothi/checksheet/ScanActivity.java
+++ CheckSheet/app/src/main/java/com/example/ngothi/checksheet/ScanActivity.java
@@ -1,30 +1,91 @@
 package com.example.ngothi.checksheet;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class ScanActivity extends AppCompatActivity {
-    EditText edittext;
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class ScanActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+    private static final String FLASH_STATE = "FLASH_STATE";
+
+    private ZXingScannerView mScannerView;
+    private EditText edittext;
+    private boolean mFlash;
+    private String mSeq;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
-        edittext =(EditText) findViewById(R.id.textSeq1);
+
+        ViewGroup contentFrame = (ViewGroup) findViewById(R.id.content_frame);
+        mScannerView = new ZXingScannerView(this);
+        contentFrame.addView(mScannerView);
+
+        edittext = (EditText) findViewById(R.id.textSeq1);
     }
-    public void Scan2(View v)
-    {
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        setTitle("Barcode Scanner");
+        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+        mScannerView.setFlash(mFlash);
+        mScannerView.setAutoFocus(true);
+        mScannerView.startCamera();
+        // Start camera on resume
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();           // Stop camera on pause
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        mSeq = result.getText();
+        Toast.makeText(this, "Contents = " + result.getText() +
+                ", Format = " + result.getBarcodeFormat().toString(), Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                edittext.setText(mSeq);
+            }
+        }, 2000);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(FLASH_STATE, mFlash);
+    }
+
+    public void toggleFlash(View v) {
+        mFlash = !mFlash;
+        mScannerView.setFlash(mFlash);
+    }
+
+    public void Scan2(View v) {
         Intent intent = new Intent(ScanActivity.this, Sheetctivity.class);
         Bundle ten_image = new Bundle();
         ten_image.putString("Seq", edittext.getText().toString());
-        intent.putExtra("GoiTen1",ten_image);
+        intent.putExtra("GoiTen1", ten_image);
         startActivity(intent);
     }
-    public void back1(View v)
-    {
+
+    public void back1(View v) {
         finish();
     }
+
+
 }
