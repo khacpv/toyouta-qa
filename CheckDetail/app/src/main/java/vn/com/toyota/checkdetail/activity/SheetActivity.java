1104b7b897729ca44b0ab49c02f3a8a240897408
Do Hoang <hoangdp90@gmail.com>
2017-03-21 23:10:54 +0700

    add Camera activity

--- CheckDetail/app/src/main/java/vn/com/toyota/checkdetail/activity/SheetActivity.java
+++ CheckDetail/app/src/main/java/vn/com/toyota/checkdetail/activity/SheetActivity.java
@@ -249,11 +249,10 @@ public class SheetActivity extends BaseActivity implements OnItemListener<ImageC
     }
 
     public void captureClick(View v) {
-        //TODO add CameraActivity
-        /*AudioManager mgr = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        AudioManager mgr = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
         mgr.setStreamMute(AudioManager.STREAM_SYSTEM, true);
         Intent intent = new Intent(SheetActivity.this, CameraActivity.class);
-        startActivityForResult(intent, REQUEST_ID_IMAGE_CAPTURE);*/
+        startActivityForResult(intent, REQUEST_ID_IMAGE_CAPTURE);
     }
 
     public static Bitmap rotateImage(Bitmap source, float angle) {
